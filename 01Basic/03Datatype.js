aeafb3f2bec60c4ea28f16470393db85381f5e9d
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-23 06:21:05 +0000

    Datatypes

---
+++ 01Basic/03Datatype.js
@@ -0,0 +1,16 @@
+let name = "ajay"//string
+let age = 12// number
+let ab = false//boolean
+let ac = Boolean
+let ad = undefined// undefiend
+let ae = Symbol// unique 
+let af = null//object 
+let d = BigInt// function
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof ab)
+console.log(typeof ac)
+console.log(typeof ad)
+console.log(typeof af)
+console.log(typeof d)
