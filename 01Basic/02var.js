8d9acfb01ef4b836245267438b32c7b45ce08d73
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-23 06:11:48 +0000

    variables

---
+++ 01Basic/02var.js
@@ -0,0 +1,19 @@
+//const accountId =3456
+let accountEmail = "ajay@gmail.com"
+var accountPassword = "123456"
+accountCity = "mandsour"
+let ajay;
+//accountId = 2 // not allowed
+
+accountEmail = "shubham@gmail.com"
+
+accountPassword ="454545"
+
+accountCity = "indore"
+
+/*
+prefer not use to var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,ajay]);
