565a5c6de9c0b8f96bd0dfee2760315582bdee76
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-24 08:04:36 +0000

    comparision operation java script

---
+++ 01Basic/operaters.js
@@ -0,0 +1,33 @@
+// let a=10
+// console.log(typeof a)
+
+// let b = String(a)
+// console.log( typeof b)
+
+// let d =" "
+// console.log(d)
+// let e = Number(d)
+// console.log(e + 1)
+
+// let ab = ""
+// let bc = Number(ab)
+// console.log( typeof bc)
+
+// console.log(a+b)
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// console.log(a*b)
+//console.log(ab)
+
+
+let str = "ajay"
+let str2 = " bairagi"
+console.log(str + str2)
+
+console.log("10" + 12)
+console.log(10 + "12")
+console.log(10 + 20 + "30")
+console.log(10 + "20" + 30)
