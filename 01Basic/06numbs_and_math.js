ed44b2a6bff6406f7eb376365375fa7f08747d64
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-25 05:28:04 +0000

    math and num

---
+++ 01Basic/06numbs_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log( typeof balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(1));
+
+const hundresh = 1000000
+//console.log(hundresh.toLocaleString('en-IN'))
+
+// math////////////
+
+
+console.log((Math.random()*2000) + 1)
