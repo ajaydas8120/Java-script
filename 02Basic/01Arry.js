8965401095138b555e4cac41f18602e2b375e979
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-27 06:00:47 +0000

    Arrys

---
+++ 02Basic/01Arry.js
@@ -0,0 +1,67 @@
+//let  arry1= [1,2,3,4,5,6,7,8]
+//console.log(arry1)
+//arry methods
+// arry1.push(122)
+// arry1.push(133)
+// arry1.pop()
+// arry1.pop()
+// console.log(arry1)
+
+// arry1.unshift(123)
+// arry1.unshift(1234)
+// arry1.shift()
+// arry1.shift()
+// console.log(arry1)
+
+
+//console.log(arry1[3])
+
+//let Arry2 = [24,22,21,20,10,"Ajy","shubham"]
+//console.log(Arry2.concat(arry1))
+//console.log(arry1.includes(100))const newArry = arry1.join()
+// console.log(arry1)
+// console.log(newArry)
+
+// const myn1 = arry1.slice(1,3)
+// console.log(myn1)
+
+// const myn2 = arry1.splice()
+//  console.log(myn2)
+
+//  const myn3 = arry1
+//  console.log(myn3)
+
+
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros =["superman","flas","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros[3][0])
+
+//  const my6=marvel_heros.concat(dc_heros)
+// console.log(my6)
+
+
+// const my7 = [...dc_heros,...marvel_heros]
+//  console.log(my7)
+
+
+// const my7 = [1,2,3,[1,3,4],7,[12,5,90,[23,34,45]]]
+// const my8 = my7.flat(Infinity)
+// console.log(my8)
+
+console.log (Array.isArray("Ajay"))
+
+console.log(Array.from("ajay"))
+
+console.log (Array.from({name:"AjAy"}))
+
+let score =100
+let score2 = 2000
+let dcore3 = 3000
+console.log(Array.of(score,score2,dcore3));
+
+
+
+
+
