ba7c9741a8403ef7f76ecabdbeb0d3df10d42d81
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-29 08:47:44 +0000

    global and local scopes

---
+++ 03Basic/02_scope.js
@@ -0,0 +1,29 @@
+// let a =10;
+// var b =20;
+// const c =30;
+// let e =5000
+
+
+// if(true){
+//     let a =10
+// var b =20
+// const d =30
+// console.log(e)
+
+
+}
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+function one(){
+    const userName = "ajay"
+
+    function two(){
+        const webSite = "youtube"
+        console.log(userName)
+    }
+    //console.log(webSite)
+    //two()
+}
+one()
