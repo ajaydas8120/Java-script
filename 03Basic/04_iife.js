2f2f9509c2cd5ba8319f418c65821e63b0216097
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-30 05:36:34 +0000

    iife java script

---
+++ 03Basic/04_iife.js
@@ -0,0 +1,15 @@
+// immediately invoked  function expression  (iife)
+
+(function Ajay(){
+    console.log(`Db connect`);
+
+// })();
+
+// (()=>{
+//     console.log(`db two is connected`);
+// })()
+
+// ( function Ajay()=>{
+//     console.log(`db two is connected`);
+// })()
+
