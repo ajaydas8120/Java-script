4b6ec6ca34fa632ff743c606b66dafc9193f68cf
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-30 07:54:14 +0000

    if else

---
+++ 04controlflow/one.js
@@ -0,0 +1,55 @@
+// const isloggedin = true
+// const temp = 50
+// if(temp >= 41){
+//     console.log("lase then 50")
+// }
+
+// const isloggedin = true
+// if(2=="2"){
+//     console.log("lase then 50")
+// }
+
+
+// const temp = 40
+// if(temp === 40){
+//     console.log("lase then 40")
+// }else
+// {
+//     console.log("greter then 40")
+// }
+
+
+
+// const score =200
+// if(score > 100){
+//     const power ="fly"
+//     console.log(`${score},this is runnig`)
+// }else
+// {
+//     console.log("is not runnig")
+// }
+
+// console.log(`user power ,${score}`)
+
+
+
+// const balance = 1000;
+// //if(balance  > 500) console.log("test")
+// if(balance < 500){
+//     console.log("lase then 500")
+// }else if(balance > 750){
+//     console.log(" lase then 750")
+// }else if(balance > 950){
+//     console.log("lase the  950")
+// }
+
+const userLoggedIN = true
+const debitCard = true
+const googlrLoggedIn = false
+const emailLoggedIn = true
+if(userLoggedIN && debitCard && 2==3){
+    console.log("allow to buy course")
+}
+if(googlrLoggedIn || emailLoggedIn){
+    console.log("user logged in")
+}
