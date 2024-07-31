e07706defab509027fd9978421b78a42771ccb78
Ajay das <125629669+ajaydas8120@users.noreply.github.com>
2024-07-31 05:10:43 +0000

    while and do while loop

---
+++ 05_forLoop/02two.js
@@ -0,0 +1,18 @@
+// let i = 0;
+// while(i<=10){
+//     console.log(`value is ${i}`)
+//     i = i+1;
+// }
+
+// let myArray = ["saktiman ","balveer","barman"]
+// let arr =0;
+// while(arr<myArray.length){
+//     console.log(`valur is ${myArray[arr]}`)
+//     arr = arr + 1;
+// }
+
+let score = 11;
+do {
+    console.log(`is vale is ${score}`)
+    score++
+}while(score<=10);
